635daea680ed6427e4f978b5e9049138b690436e
Mia-Salazar <maria.sgr@gmail.com>
2025-06-30 18:37:20 +0200

    Improve card styles

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,8 +4,8 @@ import { CardModel } from "./Card.types"
 
 const Card = ({link, linkText, title}: CardModel) => {
     return (
-        <article className="p-4 h-50 flex flex-col justify-center items-center">
-            <h3 className="mb-4">{title}</h3>
+        <article className="p-4 h-50 flex flex-col justify-center items-center rounded-sm border border-teal-500 hover:bg-teal-500 group transition">
+            <h3 className="mb-4 group-hover:text-white">{title}</h3>
             <Link href={link} className="bg-gradient-to-r from-pink-500 to-rose-500 px-4 py-2 text-white">
                 {linkText}
             </Link>
