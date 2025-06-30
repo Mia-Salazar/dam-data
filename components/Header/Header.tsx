529b9d064db64b1fb49b0e6e4b83c5c95eadac6a
Mia-Salazar <maria.sgr@gmail.com>
2025-06-30 13:04:29 +0200

    Add header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,13 @@
+const Header = ({}) => {
+    return (
+        <header className="bg-teal-500 w-full p-3 fixed">
+            <h1 className="text-white text-xl font-bold items-center flex">
+                <span className="mb-1 mr-2" aria-hidden="true">🚅</span>
+                Trenes Chuchuu
+            </h1>
+        </header>
+    )
+
+}
+
+export default Header
