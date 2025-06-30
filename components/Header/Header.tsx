87ffce9d5f685da30c5f89cf21d6e1b2738a50d9
Mia-Salazar <maria.sgr@gmail.com>
2025-06-30 22:01:07 +0200

    Add link to header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link"
+
 const Header = ({}) => {
     return (
         <header className="bg-teal-500 w-full p-3 fixed">
-            <h1 className="text-white text-xl font-bold items-center flex">
-                <span className="mb-1 mr-2" aria-hidden="true">🚅</span>
-                Trenes Chuchuu
-            </h1>
+            <Link href="/">
+                <h1 className="text-white text-xl font-bold items-center flex">
+                    <span className="mb-1 mr-2" aria-hidden="true">🚅</span>
+                    Trenes Chuchuu
+                </h1>
+            </Link>
         </header>
     )
 
