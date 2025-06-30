8dc913c956f63ecb056036aceaa4a0698922c26e
Mia-Salazar <maria.sgr@gmail.com>
2025-06-30 19:16:12 +0200

    Add components

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 import Card from './Card/Card';
 import Header from './Header/Header';
 import Loading from './Loading/Loading';
+import NoticeItem from './NoticeItem/NoticeItem';
 import Title from './Title/Title';
 
-export { Card, Header, Loading, Title };
+export { Card, Header, Loading, NoticeItem, Title };
