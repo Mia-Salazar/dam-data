39d50499ca840d226cbd63a8f927faf05eff4a1f
Mia-Salazar <maria.sgr@gmail.com>
2025-06-30 22:10:01 +0200

    Add notice page

--- app/notices/page.tsx
+++ app/notices/page.tsx
@@ -1,12 +1,26 @@
 import { getNoticesList } from "@/application/NoticesService";
-
+import { Title } from "@/components";
+import Image from "next/image";
 
 const Notices =  async() => {
     const notices = await getNoticesList()
     console.log(notices, 'aa')
+
     return (
-        <section className="">
-    avisos
+        <section>
+            <Title>Avisos</Title>
+                <figure className="mt-8 w-full h-100 object-cover">
+                    <Image 
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{ width: '100%', height: 'auto' }}
+                        alt=""
+                        src="/notice.jpg" 
+                        className="max-w-full max-h-full"
+                    />
+                </figure>
+            <h3 className="mt-6 text-xl">Actualmente hay <span className="bg-teal-500 text-white px-2">{notices.length}</span> avisos</h3>
         </section>
     );
 }
---
+++ components/NoticeItem/NoticeItem.tsx
@@ -0,0 +1,10 @@
+import { NoticesInterface } from "@/domain/notices.types"
+
+const NoticeItems = ({ paragraph, chipText, tags }:  NoticesInterface) => {
+    return (
+        <li></li>
+    )
+
+}
+
+export default NoticeItems
