f8a84577332ac862645eeedeac35f4233bc96608
Mia-Salazar <maria.sgr@gmail.com>
2025-06-30 19:00:04 +0200

    Add loading component and page

---
+++ app/notices/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from "@/components";
+
+export default function LoadingPage () {
+    return(
+        <div aria-busy="true">
+            <Loading />
+        </div>
+    )
+}
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import Card from './Card/Card';
 import Header from './Header/Header';
+import Loading from './Loading/Loading';
 import Title from './Title/Title';
 
-export { Card, Header, Title };
+export { Card, Header, Loading, Title };
