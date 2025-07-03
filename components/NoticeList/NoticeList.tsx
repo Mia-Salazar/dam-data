11eca369e769ba7c6fc6c6da0a01eb10d9f5eecb
Mia-Salazar <maria.sgr@gmail.com>
2025-07-03 11:21:49 +0200

    Reset filters

--- components/NoticeList/NoticeList.tsx
+++ components/NoticeList/NoticeList.tsx
@@ -6,25 +6,34 @@ import NoticeItems from "../NoticeItem/NoticeItem";
 import { NoticeResponseInterface } from "@/domain/notices.types";
 
 const NoticeList = ({ notices }: {notices: NoticeResponseInterface[]}) => {
-  const [filteredNotices, setFilteredNotices] = useState<NoticeResponseInterface[]>(notices)
-  const [filterName, setFilterName] = useState("España")
+    const [filteredNotices, setFilteredNotices] = useState<NoticeResponseInterface[]>(notices)
+    const [filterName, setFilterName] = useState("España")
 
-  const handleChangeFilter = (filter: string) => {
-    const newNotices = notices.filter(notice => notice.tags.find(tag => tag.text === filter))
-    setFilteredNotices(newNotices)
-    setFilterName(filter)
+    const handleChangeFilter = (filter: string) => {
+        const newNotices = notices.filter(notice => notice.tags.find(tag => tag.text === filter))
+        setFilteredNotices(newNotices)
+        setFilterName(filter)
 
-  }
+    }
+
+    const handleReset = () => {
+        setFilteredNotices(notices)
+        setFilterName("España")
+    }
 
     return (
         <>
             <h3 className="mt-6 text-xl">Actualmente hay 
-                <span className="bg-teal-500 text-white px-2 mx-2">{filteredNotices.length}</span> 
+                <span className="bg-teal-500 text-white px-2 mx-2 rounded-md">{filteredNotices.length}</span> 
                 avisos
             </h3>
-            <h4 className="mt-4">Actualmente se está filtrando por: 
-                <span className="bg-gradient-to-r from-pink-500 to-rose-500 ml-2 text-white px-1 rounded-md">{filterName}</span>
-            </h4>
+            <div className="flex justify-between">
+                <h4 className="mt-4">Actualmente se está filtrando por: 
+                    <span className="badge ml-2 p-2">{filterName}</span>
+                </h4>
+                <button className="badge px-3" onClick={handleReset}>Resetear filtros</button>
+            </div>
+
             <ul className="mt-6">
                 {filteredNotices.map(notice => 
                     <NoticeItems 
