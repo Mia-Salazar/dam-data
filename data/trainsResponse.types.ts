4d6e7e0a53806e9e38e584bf4573c83190090d42
Mia-Salazar <maria.sgr@gmail.com>
2025-07-03 13:14:08 +0200

    Interface for api response

---
+++ data/trainsResponse.types.ts
@@ -0,0 +1,43 @@
+interface FieldsInterface {
+    type: string;
+    id: string;
+}
+
+interface TrainInterface {
+    _id: number;
+    MODELO: string;
+    PROPOSITO: string;
+    "ANCHO VIA": string;
+    "VELOCIDAD MAXIMA": string;
+    MOTORES: string;
+    "POTENCIA TOTAL": string;
+    TENSION: string;
+    "MASA SIN CARGA": string;
+    "LONGITUD TOTAL": string;
+    "PLAZAS SENTADAS": string;
+    SEÑALIZACION: string;
+    CONSTRUCTOR: string;
+    "UNIDADES CONSTRUIDAS": number;
+    FOTOS: string;
+    VIDEO: string;
+    PRESTACIONES: string;
+    "FICHA TECNICA": string;
+}
+
+export interface TrainsResponseInterface {
+    help: string;
+    result: {
+        fields: FieldsInterface[]
+        include_total: boolean;
+        limit: number;
+        records: TrainInterface[];
+        records_format: string;
+        resource_id: string;
+        total: number;
+        _links: {
+            next: string;
+            start: string;
+        }
+    };
+    success: boolean;
+}
